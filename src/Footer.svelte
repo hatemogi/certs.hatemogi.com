8d9376b0bcee8a43e375b6ae6de0bc2b2bfdf3d5
Daehyun Kim <hatemogi@gmail.com>
2020-12-30 22:56:18 +0900

    푸터 링크 수정

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -2,7 +2,7 @@
 	<div class="content has-text-centered">
 	  <p>
         hatemogi
-        <a href="https://github.com/hatemogi/certificates.hatemogi.com">
+        <a href="https://github.com/hatemogi/certs.hatemogi.com">
             <i class="fab fa-github"></i>
         </a>
 	  </p>
